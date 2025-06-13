58f6c8d82be83fc9bdea13570c4f5ed83f7c6b45
yanal-halabi <halabiyanal@gmail.com>
2025-06-13 19:56:47 +0200

    fix delete flat bug

--- dashboard-ui/vite.config.js
+++ dashboard-ui/vite.config.js
@@ -5,6 +5,9 @@ export default defineConfig({
     plugins: [react()],
     server: {
         port: 5173,
+        proxy: {
+            '/api': 'http://localhost:8082',
+        },
     },
     build: {
         outDir: 'dist',
